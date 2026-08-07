017fde96305042334f531b4838d48ad4212865bc
Abhishek Kaushik <abhishekkaushik001@gmail.com>
2026-08-07 18:12:33 +0530

    fix admin dashboard mobile responsive

--- src/components/Dashboard/Others/AllTask.jsx
+++ src/components/Dashboard/Others/AllTask.jsx
@@ -1,59 +1,45 @@
 function AllTask({ employees }) {
   return (
-    <div className="w-full mt-5 overflow-x-auto">
+    <div className="w-full mt-5">
 
       {/* Header */}
-      <div className="min-w-[600px] flex justify-between bg-gray-800 px-4 py-3 rounded">
-        
+      <div className="flex justify-between items-center bg-gray-800 px-4 py-3 rounded">
         <h3 className="w-1/5 text-white font-medium">
-          Employee Name
+          Employee
         </h3>
 
         <h3 className="w-1/5 text-white font-medium">
-          New Task
+          New
         </h3>
 
         <h3 className="w-1/5 text-white font-medium">
-          Active Task
+          Active
         </h3>
 
         <h3 className="w-1/5 text-white font-medium">
-          Completed
+          Complete
         </h3>
 
         <h3 className="w-1/5 text-white font-medium">
           Failed
         </h3>
-
       </div>
 
 
       {employees.map((emp, index) => {
 
-        const newTask = emp.tasks.filter(
-          (task) => task.newTask
-        ).length;
-
-        const activeTask = emp.tasks.filter(
-          (task) => task.active
-        ).length;
-
-        const completedTask = emp.tasks.filter(
-          (task) => task.completed
-        ).length;
-
-        const failedTask = emp.tasks.filter(
-          (task) => task.failed
-        ).length;
-
+        const newTask = emp.tasks.filter(task => task.newTask).length;
+        const activeTask = emp.tasks.filter(task => task.active).length;
+        const completedTask = emp.tasks.filter(task => task.completed).length;
+        const failedTask = emp.tasks.filter(task => task.failed).length;
 
         return (
           <div
             key={index}
-            className="min-w-[600px] border-2 border-emerald-500 py-3 px-4 flex justify-between rounded mb-2"
+            className="flex justify-between items-center border-2 border-emerald-500 py-3 px-4 rounded mb-2"
           >
 
-            <h2 className="w-1/5 text-white truncate">
+            <h2 className="w-1/5 text-white text-sm">
               {emp.name}
             </h2>
 
