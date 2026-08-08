d8cc35c07ab5550f6da25bdce1b212bbd769e0a8
Abhishek Kaushik <abhishekkaushik001@gmail.com>
2026-08-08 11:34:22 +0530

    fix header logout button alignment

--- src/components/Dashboard/Others/Header.jsx
+++ src/components/Dashboard/Others/Header.jsx
@@ -1,23 +1,30 @@
 function Header({ user, setUser }) {
+
   const logOutUser = () => {
     setUser(null);
   };
 
   return (
-    <div className="flex items-end justify-between">
-      <h1 className="text-2xl font-medium text-white">
-        Good Morning 👋 <br />
-        <span className="text-3xl font-semibold">
+    <div className="flex justify-between items-start">
+
+      <div>
+        <h1 className="text-2xl sm:text-3xl font-medium text-white">
+          Good Morning 👋
+        </h1>
+
+        <h2 className="text-3xl sm:text-4xl font-semibold text-white">
           {user?.name || "Admin"}
-        </span>
-      </h1>
+        </h2>
+      </div>
+
 
       <button
         onClick={logOutUser}
-        className="bg-red-600 text-lg font-medium text-white px-5 py-2 rounded-sm"
+        className="bg-red-600 text-lg font-medium text-white px-5 py-2 rounded-lg"
       >
         Log Out
       </button>
+
     </div>
   );
 }
