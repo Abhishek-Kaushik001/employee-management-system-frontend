e620023bfaa833abfffb437dc4fc777d18e45e8c
Abhishek Kaushik <abhishekkaushik001@gmail.com>
2026-08-08 11:25:18 +0530

    fix dashboard layout issue

--- src/components/Dashboard/Others/TaskListNumber.jsx
+++ src/components/Dashboard/Others/TaskListNumber.jsx
@@ -6,29 +6,42 @@ function TaskListNumber({ user }) {
   const failedTask = user.tasks.filter((task) => task.failed).length;
 
   return (
-    <div className="grid grid-cols-2 gap-4">
 
-      <div className="rounded-xl py-6 px-9 bg-blue-400">
-        <h2 className="text-3xl font-semibold">{newTask}</h2>
-        <h3 className="text-xl font-medium">New Task</h3>
+    <div className="grid grid-cols-2 gap-3">
+
+      <div className="rounded-xl py-4 px-4 bg-blue-400">
+        <h2 className="text-2xl font-semibold">{newTask}</h2>
+        <h3 className="text-sm sm:text-xl font-medium">
+          New Task
+        </h3>
       </div>
 
-      <div className="rounded-xl py-6 px-9 bg-yellow-400">
-        <h2 className="text-3xl font-semibold">{activeTask}</h2>
-        <h3 className="text-xl font-medium">Active Task</h3>
+
+      <div className="rounded-xl py-4 px-4 bg-yellow-400">
+        <h2 className="text-2xl font-semibold">{activeTask}</h2>
+        <h3 className="text-sm sm:text-xl font-medium">
+          Active Task
+        </h3>
       </div>
 
-      <div className="rounded-xl py-6 px-9 bg-green-400">
-        <h2 className="text-3xl font-semibold">{completedTask}</h2>
-        <h3 className="text-xl font-medium">Completed</h3>
+
+      <div className="rounded-xl py-4 px-4 bg-green-400">
+        <h2 className="text-2xl font-semibold">{completedTask}</h2>
+        <h3 className="text-sm sm:text-xl font-medium">
+          Completed
+        </h3>
       </div>
 
-      <div className="rounded-xl py-6 px-9 bg-red-400">
-        <h2 className="text-3xl font-semibold">{failedTask}</h2>
-        <h3 className="text-xl font-medium">Failed Task</h3>
+
+      <div className="rounded-xl py-4 px-4 bg-red-400">
+        <h2 className="text-2xl font-semibold">{failedTask}</h2>
+        <h3 className="text-sm sm:text-xl font-medium">
+          Failed Task
+        </h3>
       </div>
 
     </div>
+
   );
 }
 
